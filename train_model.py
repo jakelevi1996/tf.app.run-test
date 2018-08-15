dd40c5209df9399fb8d0330fafca31a678d815f1
Jake Levi <jakelevi1996@gmail.com>
2018-08-15 12:45:53 +0100

    Added train_model.py

---
+++ train_model.py
@@ -0,0 +1,20 @@
+print("Starting imports...")
+import tensorflow as tf
+
+tf.app.flags.DEFINE_float("learning_rate", 0.001, "[0.001] Learning rate")
+tf.app.flags.DEFINE_string("name", "standard", "[standard] Name of the model")
+
+FLAGS = tf.app.flags.FLAGS
+
+def train():
+    print("Model name: ", FLAGS.name)
+    print("Learning rate: ", FLAGS.learning_rate)
+
+
+def main(_):
+    print("In main")
+    train()
+
+
+if __name__ == "__main__":
+    tf.app.run()
