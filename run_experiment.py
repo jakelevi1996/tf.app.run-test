76964d72a43535e4ad0d1a52be6a58fcb1246270
Jake Levi <jakelevi1996@gmail.com>
2018-08-15 14:23:22 +0100

    Changed float to string: works!

--- run_experiment.py
+++ run_experiment.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 import train_model
 import sys
 
+# NB all arguments must be strings !!!
 arg_list = [
     sys.argv[0],
     "--name", "New name",
-    "--learning_rate", 0.5
+    "--learning_rate", "0.5"
 ]
 
+print(arg_list)
 tf.app.run(main=train_model.main, argv=arg_list)
